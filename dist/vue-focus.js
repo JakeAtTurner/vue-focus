cbe6d880e372f255d1d68dbe969ae673e9977ec6
Jacob Cukjati <jacob.cukjati@turner.com>
2021-10-19 17:01:35 -0400

    build

---
+++ dist/vue-focus.js
@@ -0,0 +1,41 @@
+(function (exports) { 'use strict';
+
+  // got rid of compatible check for Vue 3
+  // import Vue from 'vue';
+
+  // export var version = '2.1.0';
+
+  // var compatible = (/^2\./).test(Vue.version);
+  // if (!compatible) {
+  //   Vue.util.warn('VueFocus ' + version + ' only supports Vue 2.x, and does not support Vue ' + Vue.version);
+  // }
+
+
+  var focus = {
+    inserted: function(el, binding) {
+      if (binding.value) el.focus();
+      else el.blur();
+    },
+
+    componentUpdated: function(el, binding) {
+      if (binding.modifiers.lazy) {
+        if (Boolean(binding.value) === Boolean(binding.oldValue)) {
+          return;
+        }
+      }
+
+      if (binding.value) el.focus();
+      else el.blur();
+    },
+  };
+
+  var mixin = {
+    directives: {
+      focus: focus,
+    },
+  };
+
+  exports.focus = focus;
+  exports.mixin = mixin;
+
+})((this.VueFocus = {}));
